f3f9e410504e5fb5e12da7f0ab6f3dd522d51991
ashwink0 <ashwin.kuppahally@gmail.com>
2021-01-07 19:04:41 -0800

    Change Target

--- lib/index.d.ts
+++
@@ -1,8 +0,0 @@
-interface callObjectType {
-    startYear?: number;
-    endYear?: number;
-    corpus?: number;
-    smoothing?: number;
-}
-export declare function getNGram(ngram: string, options: callObjectType): Promise<unknown>;
-export {};
